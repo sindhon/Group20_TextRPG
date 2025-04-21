ec7db75ca494e8c9607b35f7aef557d6e6439d0a
sindhon <tpgml2356@gmail.com>
2025-04-21 18:43:39 +0900

    feat: Creature 클래스 변수 및 OnDamaged 함수 추가
    - Creature 클래스 변수 및 OnDamaged 함수 추가

--- Team20_TextRPG/TextRPG_Creature.cs
+++ Team20_TextRPG/TextRPG_Creature.cs
@@ -8,12 +8,32 @@ namespace Team20_TextRPG
 {
     partial class TextRPG_Creature 
     {
-        public int Level { get; }
-        public string Name { get; }
-        public string Job { get; }
-        public int Atk { get; }
-        public int Def { get; }
-        public int Hp { get; }
-        public int Gold { get; private set; }
+        public int Level { get; protected set; }
+        public string Name { get; protected set; }
+        public string Job { get; protected set; }
+        public int Atk { get; protected set; }
+        public int Def { get; protected set; }
+        public int Hp { get; protected set; }
+        public int Gold { get; protected set; }
+        public int DataId { get; protected set; }
+
+        public void OnDamaged(TextRPG_Creature attacker)
+        {
+            Random rand = new Random();
+            int diff = (int)Math.Ceiling((double)attacker.Atk / 10);
+            int totalDamage = rand.Next(attacker.Atk - diff, attacker.Atk + diff + 1);
+
+            Hp -= totalDamage;
+
+            if (Hp < 0)
+            {
+                Hp = 0;
+            }
+        }
+
+        public void SetInfo(int dataId)
+        {
+
+        }
     }
 }
