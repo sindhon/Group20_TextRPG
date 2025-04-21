b6f7e17e4abc3e0a17825eb19c25492e6cb4659c
Im Ye Seul <147143220+Imysss@users.noreply.github.com>
2025-04-21 18:34:36 +0900

    Reapply "Merge branch 'Dev' into MonsterSpawn"
    This reverts commit 415a4b3a3151418a3c16e30301e70d8b73f47d05.

---
+++ Team20_TextRPG/TextRPG_Creature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team20_TextRPG
+{
+    partial class TextRPG_Creature 
+    {
+        public int Level { get; }
+        public string Name { get; }
+        public string Job { get; }
+        public int Atk { get; }
+        public int Def { get; }
+        public int Hp { get; }
+        public int Gold { get; private set; }
+    }
+}
