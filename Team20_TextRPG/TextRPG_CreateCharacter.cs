6178b0aefb2294accbcbcdcca502e9008fedd9da
sindhon <tpgml2356@gmail.com>
2025-04-23 20:51:57 +0900

    refactor: 간단한 직업 설명 추가

--- Team20_TextRPG/TextRPG_CreateCharacter.cs
+++ Team20_TextRPG/TextRPG_CreateCharacter.cs
@@ -22,8 +22,8 @@ namespace Team20_TextRPG
 
             public void DisplayJob()
             {
-                Console.WriteLine($"{jobName}\n{jobDescription}");
-                Console.WriteLine($"공격력 : {jobAtk} \\ 방어력 : {jobDef} \\ 최대 체력 : {jobmaxHP} \\ 최대 마나 : {jobmaxMP} \\ 스킬 : {jobSkill}\n");
+                Console.WriteLine($"{jobName}\n"); // {jobDescription}
+                // Console.WriteLine($"공격력 : {jobAtk} \\ 방어력 : {jobDef} \\ 최대 체력 : {jobmaxHP} \\ 최대 마나 : {jobmaxMP} \\ 스킬 : {jobSkill}\n");
             }
         }
 
@@ -55,7 +55,7 @@ namespace Team20_TextRPG
                 jobDescription = "기획자가 되고 싶은 취준생",
                 jobAtk = 22,
                 jobDef = 8,
-                jobmaxHP = 90,
+                jobmaxHP = 9,
                 jobmaxMP = 50,
                 jobSkill = "기획서에는 분명히 그렇게 써 있어요"
             },
@@ -142,26 +142,30 @@ namespace Team20_TextRPG
         
         public static Job ChooseJob(string name, Job[] jobs)
         {
-            Console.WriteLine("직업을 선택해 주세요.");
-            Console.WriteLine();
-
-            for (int i = 0; i < jobs.Length; i++)
-            {
-                Console.Write($"{i + 1}. ");
-                jobs[i].DisplayJob();
-            }
-
             while (true)
             {
+                Console.Clear();
+                Console.WriteLine("직업을 선택해 주세요.");
                 Console.WriteLine();
-                bool isSuccess = int.TryParse(Console.ReadLine(), out int input);
 
-                if (isSuccess)
+                for (int i = 0; i < jobs.Length; i++)
+                {
+                    Console.Write($"{i + 1}. ");
+                    jobs[i].DisplayJob();
+                }
+            
+                Console.WriteLine();
+                bool choice = int.TryParse(Console.ReadLine(), out int input);
+
+                if (choice)
                 {
                     if (input >= 1 && input < jobs.Length + 1)
                     {
-                        Console.Clear();
-                        return jobs[input - 1];
+                        var job = jobs[input - 1];
+                        int result = Desc(job);
+                        if (result == 1) return job;
+                        else continue;
+
                     }
                 }
                 else
@@ -169,6 +173,76 @@ namespace Team20_TextRPG
                     Console.WriteLine("잘못된 입력입니다.");
                 }
             }
+        }
+
+        static int Desc(Job job)
+        {
+            Console.Clear();
+            Console.WriteLine($"직업 소개\n");
+            Console.WriteLine($"{job.jobName}\n");
+            Console.WriteLine($"공격력 : {job.jobAtk} | 방어력 : {job.jobDef} | 최대 체력 : {job.jobmaxHP} | 최대 마나 : {job.jobmaxMP}\n");
+
+            switch (job.jobName)
+            {
+                case "클라 개발자 지망생":
+                    clientSkillDesc();
+                    break;
+                case "서버 개발자 지망생":
+                    serverSkillDesc();
+                    break;
+                case "기획 지망생":
+                    plannerSkillDesc();
+                    break;
+            }
+
+            commonSkillDesc();
+
+            Console.WriteLine("해당 직업으로 하시겠습니까?");
+            Console.WriteLine("1. 예");
+            Console.WriteLine("2. 아니요\n");
+            Console.Write(">>");
+            return TextRPG_SceneManager.CheckInput(1, 2);
+        }
+
+        static void commonSkillDesc()
+        {
+            Console.WriteLine("공통 스킬\n");
+
+            Console.WriteLine("1. 어..?    소모 마나: 25");
+            Console.WriteLine("   모든 몬스터에게 공격력의 60 ~ 80% 데미지를 입힌다. 해당 스킬은 회피 불가.\n");
+        }
+
+        static void clientSkillDesc()
+        {
+            Console.WriteLine("전용 스킬\n");
+
+            Console.WriteLine("1. 요건 기획이 너무 복잡해요   소모 마나: 10");
+            Console.WriteLine("   1명의 몬스터에게 공격력의 150% ~ 200%의 데미지를 입힌다. 해당 스킬은 회피 가능\n");
+
+            Console.WriteLine("2. 서버에서 응답이 이상해요   소모 마나: 15");
+            Console.WriteLine("   랜덤으로 1명의 몬스터에게 공격력의 200% 데미지를 입힌다. 해당 스킬은 회피 불가\n");
+        }
+
+        static void serverSkillDesc()
+        {
+            Console.WriteLine("전용 스킬\n");
+
+            Console.WriteLine("1. 그건 클라이언트 쪽 문제에요   소모 마나: 10");
+            Console.WriteLine("   1명의 몬스터에게 공격력의 150% ~ 200%의 데미지를 입힌다. 해당 스킬은 회피 가능\n");
+
+            Console.WriteLine("2. 로컬에서는 잘 되는데요?   소모 마나: 15");
+            Console.WriteLine("   랜덤으로 1명의 몬스터에게 200%의 데미지를 입힌다. 해당 스킬은 회피 불가\n");
+        }
+
+        static void plannerSkillDesc()
+        {
+            Console.WriteLine("전용 스킬\n");
+
+            Console.WriteLine("1. 기획서에는 분명히 그렇게 써 있어요   소모 마나: 10");
+            Console.WriteLine("   1명의 몬스터에게 150% ~ 200%의 데미지를 입힌다. 해당 스킬은 회피 가능\n");
+
+            Console.WriteLine("2. 이건 개발 쪽에서 좀 더 확인해야 할 것 같아요   소모 마나: 15");
+            Console.WriteLine("   랜덤으로 1명의 몬스터에게 200%의 데미지를 입힌다. 해당 스킬은 회피 불가\n");
         }
     }
 }
